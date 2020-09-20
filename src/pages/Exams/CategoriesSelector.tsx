aa610950842c818a706fad2152536b93111e90f5
ky <y.keddaoui@gmail.com>
2020-09-20 07:10:41 +0100

    category selector fixes

--- src/pages/Exams/CategoriesSelector.tsx
+++ src/pages/Exams/CategoriesSelector.tsx
@@ -1,13 +1,12 @@
-import { Form, Input, Button, Space, Row, Col, Select } from 'antd';
 import React, { useEffect } from 'react';
+import { Form, Input, Button, Space, Row, Col, Select } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { RootState } from '@/typings';
 import { CATEGORIES_ACTIONS, useDispatch, useSelector } from 'umi';
 
-const CategorieSelector = () => {
+const CategoriesSelector = () => {
   const { allCategories } = useSelector((state: RootState) => state.categories);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch({
       type: CATEGORIES_ACTIONS.GET_ALL,
@@ -19,68 +18,74 @@ const CategorieSelector = () => {
   };
 
   return (
-    <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
-      <Row justify="center">
-        <Form.List name="categories">
-          {(fields, { add, remove }) => {
-            return (
-              <Col span={12}>
-                {fields.map((field) => (
-                  <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="start">
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'first']}
-                      fieldKey={[field.fieldKey, 'first']}
-                      rules={[{ required: true, message: 'Missing first name' }]}
+    <Row justify="center">
+      <Col span={12}>
+        <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
+          <Form.List name="categories">
+            {(fields, { add, remove }) => {
+              return (
+                <div>
+                  {fields.map((field) => (
+                    <Space
+                      key={field.key}
+                      style={{ display: 'flex', marginBottom: 8 }}
+                      align="start"
                     >
-                      <Select placeholder="select category...">
-                        {allCategories.map(({ name }) => (
-                          <Select.Option key={name} value={name}>
-                            {name}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </Form.Item>
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'last']}
-                      fieldKey={[field.fieldKey, 'last']}
-                      rules={[{ required: true, message: 'Missing last name' }]}
-                    >
-                      <Input placeholder="Weight" type="number" />
-                    </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'category']}
+                        fieldKey={[field.fieldKey, 'category']}
+                        rules={[{ required: true }]}
+                      >
+                        <Select placeholder="select category...">
+                          {allCategories.map(({ name }) => (
+                            <Select.Option key={name} value={name}>
+                              {name}
+                            </Select.Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'weight']}
+                        fieldKey={[field.fieldKey, 'weight']}
+                        rules={[{ required: true }]}
+                      >
+                        <Input type="number" placeholder="weight" />
+                      </Form.Item>
 
-                    <MinusCircleOutlined
+                      <MinusCircleOutlined
+                        onClick={() => {
+                          remove(field.name);
+                        }}
+                      />
+                    </Space>
+                  ))}
+
+                  <Form.Item>
+                    <Button
+                      type="dashed"
                       onClick={() => {
-                        remove(field.name);
+                        add();
                       }}
-                    />
-                  </Space>
-                ))}
+                      block
+                    >
+                      <PlusOutlined /> Add category
+                    </Button>
+                  </Form.Item>
+                </div>
+              );
+            }}
+          </Form.List>
 
-                <Form.Item>
-                  <Button
-                    type="dashed"
-                    onClick={() => {
-                      add();
-                    }}
-                    block
-                  >
-                    <PlusOutlined /> Add Category
-                  </Button>
-                </Form.Item>
-              </Col>
-            );
-          }}
-        </Form.List>
-      </Row>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>{' '}
+      </Col>
+    </Row>
   );
 };
-
-export default CategorieSelector;
+export default CategoriesSelector;
