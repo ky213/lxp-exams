e463e3efaa3e111cd66cd0c1cc350740ac142006
ky <y.keddaoui@gmail.com>
2020-12-13 11:14:49 +0100

    exam delivery

--- src/pages/Exams/ExamDelivery.tsx
+++ src/pages/Exams/ExamDelivery.tsx
@@ -1,13 +1,54 @@
 import { RootState } from '@/typings';
 import { Button, Card, Descriptions, Result, Tag } from 'antd';
+import { isEmpty } from 'lodash';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { Survey } from 'survey-react';
+import * as Survey from 'survey-react';
 import { ExamResult, EXAMS_ACTIONS, useDispatch, useParams, useSelector } from 'umi';
+
+const ExamResultView = ({ result }: any) => {
+  const { currentExam } = useSelector((state: RootState) => state.exams);
+  const survey = new Survey.Model(currentExam);
+
+  survey.onComplete.add(function (result) {
+    // document.querySelector('#surveyResult').textContent =
+    //   'Result JSON:\n' + JSON.stringify(result.data, null, 3);
+  });
+
+  survey.data = result;
+
+  survey.mode = 'display';
+  survey.questionsOnPageMode = 'singlePage';
+  survey.showNavigationButtons = 'none';
+  survey.showProgressBar = 'off';
+  survey.showTimerPanel = 'none';
+  survey.maxTimeToFinishPage = 0;
+  survey.maxTimeToFinish = 0;
+  survey.onAfterRenderQuestion.add(function (currentExam, options) {
+    var span = document.createElement('span');
+    var isCorrect = options.question.isAnswerCorrect();
+    span.innerHTML = isCorrect ? 'Correct' : 'Incorrect';
+    span.style.color = isCorrect ? 'green' : 'red';
+    var header = options.htmlElement.querySelector('h5');
+    if (!isCorrect) {
+      header.style.backgroundColor = 'salmon';
+      var radio = options.htmlElement.querySelector(
+        'input[value="' + options.question.correctAnswer + '"]',
+      );
+      if (!!radio) {
+        radio.parentElement.style.color = 'green';
+      }
+    }
+    header.appendChild(span);
+  });
+
+  return <Survey.Survey model={survey} />;
+};
 
 const ExamDelivery = () => {
   const { currentExam } = useSelector((state: RootState) => state.exams);
   const [startExam, setStartExam] = useState(false);
+  const [finalResult, setFinalResult] = useState({});
   const { examId }: { examId: string } = useParams();
   const dispatch = useDispatch();
 
@@ -35,6 +76,8 @@ const ExamDelivery = () => {
       type: EXAMS_ACTIONS.SAVE_RESULT,
       payload,
     });
+
+    setFinalResult(result.data);
   };
 
   const json = {
@@ -74,7 +117,8 @@ const ExamDelivery = () => {
   return (
     <Card>
       {!startExam && <Opening />}
-      {currentExam && startExam && <Survey json={json} onComplete={handleOnComplete} />}
+      {currentExam && startExam && <Survey.Survey json={json} onComplete={handleOnComplete} />}
+      {!isEmpty(finalResult) && <ExamResultView result={finalResult} />}
     </Card>
   );
 };
